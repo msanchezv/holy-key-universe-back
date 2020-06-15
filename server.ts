560ef824e73f5fab43bc562fadfad74db2d34687
Sonia <sonia.bejar017@gmail.com>
2020-06-15 20:44:13 +0200

    Migrate to TypeScript

--- server.ts
+++ server.ts
@@ -1,9 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("mongodb");
-
-const app = express();
-app.use(bodyParser.json());
+import * as mongodb from "mongodb";
+import app from "./index";
 
 let db;
 
@@ -20,8 +16,8 @@ mongodb.MongoClient.connect(process.env.MONGO_URI || "mongodb://localhost:27017/
         res.send('Welcome to Holy Key Universe');
     });
 
-    const server = app.listen(process.env.PORT || 8080, function () {
-        const port = server.address().port;
-        console.log("App now running on port", port);
+    const PORT = process.env.PORT || 8080
+    app.listen(PORT, function () {
+        console.log("App now running on port", PORT);
     });
 });
