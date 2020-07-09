63c3e4ec3581ac09fc76f1f860c40c54ccf04296
Sonia <sonia.bejar017@gmail.com>
2020-07-09 16:54:20 +0200

    Modify validation screen POST #24

--- src/api/components/screen/screen.validator.ts
+++ src/api/components/screen/screen.validator.ts
@@ -4,6 +4,19 @@ export const SCREEN_SCHEMA: Schema = {
     type: "object",
     properties: {
         title: {type: "string"},
+        exercise: {
+            type: "object",
+            properties: {
+                mode: {
+                    type: "string",
+                    pattern: "^(openanswer|fill)$"
+                },
+                statement: {type: "string"},
+                card: {type: "string"}
+            },
+            additionalProperties: false,
+            required: ["mode, card"]
+        }
     },
     patternProperties: {
         "^(text|image|diagram)$": {type: "string"},
@@ -17,6 +30,21 @@ export const SCREEN_SCHEMA: Schema = {
             type: "array",
             items: {
                 type: "object",
+                properties: {
+                    exercise: {
+                        type: "object",
+                        properties: {
+                            mode: {
+                                type: "string",
+                                pattern: "^(openanswer|fill)$"
+                            },
+                            statement: {type: "string"},
+                            card: {type: "string"}
+                        },
+                        additionalProperties: false,
+                        required: ["mode", "card"]
+                    }
+                },
                 patternProperties: {
                     "^(text|image|diagram)$": {type: "string"},
                     "^(row|column)$": {"$ref": "#/definitions/composite"},
